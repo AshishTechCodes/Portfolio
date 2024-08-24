3422bf874a32d03fffdaebe73a71a31a80c5698e
ashish.gupta@64 <shivkumariguptaashish@gmail.com>
2024-08-24 20:57:54 +0530

    update

---
+++ src/Components/React Router_or_Routing/Path_Routes_Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+const Path_Routes_Routes = () => {
+  return (
+    <div>Path_Routes_Routes</div>
+  )
+}
+
+export default Path_Routes_Routes
